928c4dc741cf53d86b3f6a73c8c1bb034248709c
Krish Chopra <krish.chopra23@gmail.com>
2023-12-20 02:03:24 -0500

    first couple changes

--- InvoiceManager/Other/InvoiceManagerApp.swift
+++ InvoiceManager/Other/InvoiceManagerApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct InvoiceManagerApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
