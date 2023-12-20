5dfb161cbb08ebdcf662acf3c83be76c39a0c986
Krish Chopra <krish.chopra23@gmail.com>
2023-12-20 01:42:40 -0500

    Initial Commit

---
+++ InvoiceManager/InvoiceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceManagerApp.swift
+//  InvoiceManager
+//
+//  Created by Krish Chopra on 2023-12-20.
+//
+
+import SwiftUI
+
+@main
+struct InvoiceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
